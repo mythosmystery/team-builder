a029f15e96f95d01733fde7f95f02c29ad50843e
Hunter Barton <axiysfire@gmail.com>
2021-04-24 02:08:24 -0500

    moved user input prompts to new js file

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const Manager = require("./lib/manager");
 const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
-const Team = require("./lib/team")
+const Team = require("./lib/team");
+const Prompt = require("./lib/prompt");
 const fs = require("fs");
 const util = require("util");
 const inquirer = require("inquirer");
 
 const writeFileAsync = util.promisify(fs.writeFile);
-
+const prompt = new Prompt();
 const employees = [];
 
 function init() {
@@ -32,19 +33,19 @@ function menu() {
       });
 }
 function getManager() {
-   managerPrompt().then((res) => {
+   prompt.managerPrompt().then((res) => {
       employees.push(new Manager(res.name, res.id, res.email, res.officeNum));
       menu();
    });
 }
 function getIntern() {
-   internPrompt().then((res) => {
+   prompt.internPrompt().then((res) => {
       employees.push(new Intern(res.name, res.id, res.email, res.school));
       menu();
    });
 }
 function getEngineer() {
-   engineerPrompt().then((res) => {
+   prompt.engineerPrompt().then((res) => {
       employees.push(new Engineer(res.name, res.id, res.email, res.github));
       menu();
    });
@@ -52,89 +53,11 @@ function getEngineer() {
 function generateTeam() {
    console.log("generating Team...");
    const team = new Team();
-   generateHTML(team);             
+   generateHTML(team);
 }
-function generateHTML(team){    
-    const HTML = team.createBody(team.createCards(employees));
-    //console.log(HTML);
-    writeFileAsync("index.html", HTML)
-    .then(() => console.log("write ok"))
-    .catch((err) => console.error(err)); 
-}
-function printEmployees() {
-   for (const employee of employees) {
-      console.log(employee);
-   }
-}
-function internPrompt() {
-   return inquirer.prompt([
-      {
-         type: "input",
-         message: "What is this intern's name?",
-         name: "name",
-      },
-      {
-         type: "number",
-         message: "What is this intern's employee ID?",
-         name: "id",
-      },
-      {
-         type: "input",
-         message: "What is their email?",
-         name: "email",
-      },
-      {
-         type: "input",
-         message: "What school did they attend?",
-         name: "school",
-      },
-   ]);
-}
-function engineerPrompt() {
-   return inquirer.prompt([
-      {
-         type: "input",
-         message: "What is this engineer's name?",
-         name: "name",
-      },
-      {
-         type: "number",
-         message: "What is this engineer's employee ID?",
-         name: "id",
-      },
-      {
-         type: "input",
-         message: "What is their email?",
-         name: "email",
-      },
-      {
-         type: "input",
-         message: "What is their github username?",
-         name: "github",
-      },
-   ]);
-}
-function managerPrompt() {
-   return inquirer.prompt([
-      {
-         type: "input",
-         message: "What is the team manager's name?",
-         name: "name",
-      },
-      {
-         type: "number",
-         message: "What is this team manager's employee ID?",
-         name: "id",
-      },
-      {
-         type: "input",
-         message: "What is their email?",
-         name: "email",
-      },
-      {
-         type: "number",
-         message: "What is their office number?",
-         name: "officeNum",
-      },
-   ]);
+function generateHTML(team) {
+   const HTML = team.createBody(team.createCards(employees));
+   writeFileAsync("index.html", HTML)
+      .then(() => console.log("write ok"))
+      .catch((err) => console.error(err));
 }
